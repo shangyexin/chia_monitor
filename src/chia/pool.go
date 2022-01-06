99d50708c59f9d57b4fef9c2e77e8b4c939efab7
yasin <yexin.shang@gmail.com>
2022-01-06 09:20:22 +0800

    feat: 矿池收益增加电池

--- src/chia/pool.go
+++ src/chia/pool.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/url"
+	"strconv"
 
 	"github.com/robfig/cron"
 	log "github.com/sirupsen/logrus"
@@ -14,7 +15,9 @@ import (
 )
 
 const XCHPoolDailyEarningUrl = "https://farmer.xchpool.io/api/xchpool/farmer/earnings/daily"
+const DPoolRewardUrl = "https://vip.dpool.cc:9999/queryRewardRecord"
 
+//XCHPoolEarning XCHPool收益
 type XCHPoolEarning struct {
 	Success bool   `json:"success"`
 	Message string `json:"message"`
@@ -24,6 +27,30 @@ type XCHPoolEarning struct {
 		Amount float64 `json:"amount"`
 	} `json:"result"`
 	Timestamp int64 `json:"timestamp"`
+}
+
+// DpoolRewardRequest 电池收益请求
+type DpoolRewardRequest struct {
+	LauncherID string `json:"launcherId"`
+	Days       int    `json:"days"`
+}
+
+// DpoolRewardRecord 电池收益记录
+type DpoolRewardRecord struct {
+	Code int `json:"code"`
+	Data []struct {
+		Amount      string `json:"amount"`
+		BloackIndex string `json:"bloackIndex"`
+		CoinID      string `json:"coin_id"`
+		CreateTime  string `json:"createTime"`
+		ID          int    `json:"id"`
+		LauncherID  string `json:"launcherId"`
+		Points      int    `json:"points"`
+		Portion     string `json:"portion"`
+		Status      int    `json:"status"`
+		Timestamp   int    `json:"timestamp"`
+	} `json:"data"`
+	Message string `json:"message"`
 }
 
 // getXCHPoolEarning 获取XCHPool收益
@@ -49,6 +76,34 @@ func getXCHPoolEarning() (xchPoolEarning XCHPoolEarning, err error) {
 	err = json.Unmarshal(resp, &xchPoolEarning)
 
 	return xchPoolEarning, err
+}
+
+// getDpoolRewardRecord 获取电池收益
+func getDpoolRewardRecord() (dpoolRewardRecord DpoolRewardRecord, err error) {
+	//获取配置文件
+	cfg := config.GetConfig()
+
+	//电池收益请求
+	dpoolRewardRequest := DpoolRewardRequest{
+		LauncherID: cfg.Monitor.LauncherId,
+		Days:       30,
+	}
+	log.Infof("dpoolRewardRequest: %+v", dpoolRewardRequest)
+	resp, err := utils.Post(DPoolRewardUrl, dpoolRewardRequest, "application/json")
+	if err != nil {
+		log.Errorf("Get Dpool reward failed: %+v", err)
+		return
+	}
+	//反序列化json
+	log.Debug("dpoolRewardResponse: ", string(resp))
+	err = json.Unmarshal(resp, &dpoolRewardRecord)
+	if err != nil {
+		log.Errorf("Unmarshal Dpool reward failed: %+v", err)
+	} else {
+		log.Infof("Get Dpool Reward Record success:%+v", dpoolRewardRecord)
+	}
+
+	return dpoolRewardRecord, err
 }
 
 //MonitorPoolEarning 监控矿池收益
@@ -80,6 +135,27 @@ func MonitorPoolEarning(poolName string) {
 				yesterdayEarning := xchPoolEarning.Result[len(xchPoolEarning.Result)-2].Amount
 				todayEarning := xchPoolEarning.Result[len(xchPoolEarning.Result)-1].Amount
 				detail = fmt.Sprintf("%s，昨日收益：%.5f，今日当前收益：%.5f", poolName, yesterdayEarning, todayEarning)
+				remark = "获取矿池收益成功"
+			}
+		case "Dpool":
+			var yesterdayReward string
+			dpoolRewardRecord, err := getDpoolRewardRecord()
+			if err != nil {
+				detail = fmt.Sprintf("获取Dpool收益错误：%s", err.Error())
+				remark = "获取矿池收益错误"
+			} else if dpoolRewardRecord.Code != 0 {
+				detail = fmt.Sprintf("获取XCHPool收益失败：%s", dpoolRewardRecord.Message)
+				remark = "获取矿池收益失败"
+			} else {
+				if len(dpoolRewardRecord.Data) > 1{
+					yesterdayReward = dpoolRewardRecord.Data[1].Amount
+				}
+				todayReward :=  dpoolRewardRecord.Data[0].Amount
+				yesterdayRewardInt, _ := strconv.Atoi(yesterdayReward)
+				yesterdayRewardFloat, _ := strconv.ParseFloat(fmt.Sprintf("%.12f", float64(yesterdayRewardInt)/float64(1000000000000)), 64)
+				todayRewardInt, _ := strconv.Atoi(todayReward)
+				todayRewardFloat, _ := strconv.ParseFloat(fmt.Sprintf("%.12f", float64(todayRewardInt)/float64(1000000000000)), 64)
+				detail = fmt.Sprintf("%s，昨日收益：%.5f，今日当前收益：%.5f", poolName, yesterdayRewardFloat, todayRewardFloat)
 				remark = "获取矿池收益成功"
 			}
 		default:
